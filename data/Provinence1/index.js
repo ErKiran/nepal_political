fdf0eaa4a60a361a0d40d094dadb9025a6c11f27
Kiran Adhikari <kiruu1238@gmail.com>
2019-05-19 13:55:41 +0545

    Data Cleaned

--- data/Provinence1/index.js
+++ data/Provinence1/index.js
@@ -1,11 +1,5 @@
-const Mun = require('./Municipalities.json');
 const fs = require('fs');
+const Mun = require('./Municipalities');
 
-//delete json.map
-let todelete = "Rank";
-
-const trimmed = Mun.map(({ Rank, Website, ...rest }) => rest)
-console.log(trimmed);
-
-fs.writeFileSync('trimmed.json', JSON.stringify(trimmed, null, 2));
-//console.log(Mun.map(i => i.Nepali));
+const trimmed = Mun.map(({ Rank, Website, ...rest }) => rest);
+fs.writeFileSync('Trimed.json', JSON.stringify(trimmed, null, 2));
