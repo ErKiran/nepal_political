07ae7e097392e699b95e34405fadf5408b62e195
Kiran Adhikari <kiruu1238@gmail.com>
2019-05-25 17:27:59 +0545

    NPM module complete

--- data/json/Nepal/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const District = require('./District.json');
-
-for (i = 0; i < District.length; i++) {
-    District[i].Name = District[i].Districts
-    delete District[i].Districts;
-}
-console.log(District);
