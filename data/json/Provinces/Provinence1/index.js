83cfe75c21d286beea4d84904f2b0878f3d9b7c7
Kiran Adhikari <kiruu1238@gmail.com>
2019-05-25 18:08:09 +0545

    Files removed for npm package

--- data/json/Provinces/Provinence1/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const Mun = require('./Municipalities');
-
-
-//const trimmed = Mun.map(({ h, ...rest }) => rest);
-const filter = Mun.filter(i => i.Province == 'Sudurpashchim')
-fs.writeFileSync('../SudurPashchim/SubMetro.json', JSON.stringify(filter, null, 2));
